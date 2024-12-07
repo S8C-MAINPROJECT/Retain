6ebf9c4d21fac7128b5d95e677969ca89f7c7694
Gokul Krishna B <97348670+Gokul-Krishna-B-7@users.noreply.github.com>
2024-12-07 09:56:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import "./App.css";
-import Header from "./components/header";
-function App() {
-  return <Header />;
-}
+import React, { useState } from 'react';
+import { Box, Card } from '@mui/material';
+import CardStack from './components/Card/CardStack';
+import DifficultyChooser from './components/DifficultyChooser/DifficultyChooser';
+import ProgressBar from './components/ProgressBar/ProgressBar';
+
+const App: React.FC = () => {
+  // const [progress, setProgress] = useState(30); // Example progress
+
+  return (
+    <div className="App">
+      {/* <ProgressBar progress={progress} /> */}
+      <CardStack />
+      <DifficultyChooser />
+    </div>
+  );
+};
 
 export default App;
+
+    // <Box
+    //   sx={{
+    //     background: "#f3f3f3",
+    //     height: "100vh",
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     alignItems: "center",
+    //     justifyContent: "center",
+    //     padding: "20px",
+    //   }}
+    // >
+    //   {/* <ProgressBar progress={progress} /> */}
+    //   <CardStack />
+    // </Box>
