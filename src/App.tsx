58a85559587b6dc206fe79997a8080a34ccaa902
Sourav U P <souravputhanveedu@gmail.com>
2024-12-29 10:14:44 +0530

    signUp added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Card from './pages/Card/Card';
-import Login from './pages/Login/Login';
-import Home from './pages/Home/Home';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Card from "./pages/Card/Card";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import SignUp from "./pages/Signup/SignUp";
 
 const App: React.FC = () => {
   return (
@@ -18,6 +18,8 @@ const App: React.FC = () => {
           <Route path="/home" element={<Home />} />
           {/* Route for Card Page */}
           <Route path="/card" element={<Card />} />
+          {/* Route for Signup Page */}
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </div>
     </Router>
@@ -26,20 +28,18 @@ const App: React.FC = () => {
 
 export default App;
 
-
-
 // import { Box, Card } from '@mui/material';
-    // <Box
-    //   sx={{
-    //     background: "#f3f3f3",
-    //     height: "100vh",
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     alignItems: "center",
-    //     justifyContent: "center",
-    //     padding: "20px",
-    //   }}
-    // >
-    //   {/* <ProgressBar progress={progress} /> */}
-    //   <CardStack />
-    // </Box>
+// <Box
+//   sx={{
+//     background: "#f3f3f3",
+//     height: "100vh",
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     padding: "20px",
+//   }}
+// >
+//   {/* <ProgressBar progress={progress} /> */}
+//   <CardStack />
+// </Box>
