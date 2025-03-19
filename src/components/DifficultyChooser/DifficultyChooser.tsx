dabc0261e8500a24639240b4511ea1bf182b5a9e
Gokul-Krishna-B-7 <gokulkrishnabiju@gmail.com>
2025-03-20 00:53:51 +0530

    line to be added in cards page

--- src/components/DifficultyChooser/DifficultyChooser.tsx
+++ src/components/DifficultyChooser/DifficultyChooser.tsx
@@ -1,31 +1,37 @@
-import "./DifficultyChooser.css";
+import React from 'react';
+import './DifficultyChooser.css';
 
 interface DifficultyChooserProps {
-  handleNext: () => void; // The type of the handleNext function
+  onDifficultySelect: (difficulty: number) => void; // Accepts a function
 }
 
-const DifficultyChooser: React.FC<DifficultyChooserProps> = ({
-  handleNext,
-}) => {
+const DifficultyChooser: React.FC<DifficultyChooserProps> = ({ onDifficultySelect }) => {
   return (
-    <div>
-      <div className="difficultyOptions">
-        <button className="difficultyBtn hard" onClick={() => handleNext()}>
-          <span className="difficultyTime">15 min</span>
-          <br></br>
-          <span className="difficultyMeasure">Again</span>
-        </button>
-        <button className="difficultyBtn medium" onClick={() => handleNext()}>
-          <span className="difficultyTime">4 day</span>
-          <br></br>
-          <span className="difficultyMeasure">Easy</span>
-        </button>
-        <button className="difficultyBtn easy" onClick={() => handleNext()}>
-          <span className="difficultyTime">1 day</span>
-          <br></br>
-          <span className="difficultyMeasure">Good</span>
-        </button>
+    <div className="difficulty-chooser">
+      <h2>Select your difficulty level here</h2>
+      <div className="difficulty-options">
+        <div className="difficulty-option" onClick={() => onDifficultySelect(1)}>
+          <span role="img" aria-label="Again">😢</span>
+          <p>Again</p>
+          <p>15 min</p>
+        </div>
+        <div className="difficulty-option" onClick={() => onDifficultySelect(2)}>
+          <span role="img" aria-label="Hard">😥</span>
+          <p>Hard</p>
+          <p>15 min</p>
+        </div>
+        <div className="difficulty-option selected" onClick={() => onDifficultySelect(3)}>
+          <span role="img" aria-label="Good">😊</span>
+          <p>Good</p>
+          <p>1 day</p>
+        </div>
+        <div className="difficulty-option" onClick={() => onDifficultySelect(4)}>
+          <span role="img" aria-label="Easy">😌</span>
+          <p>Easy</p>
+          <p>4 days</p>
+        </div>
       </div>
+      <button className="previous-button">Go to previous</button>
     </div>
   );
 };
